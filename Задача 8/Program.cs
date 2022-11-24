b741e32d4b39786199c1038cd8726ceff119db8a
Albert <albert.obychenie@moe.ru>
2022-11-24 14:40:34 +0500

    Закончил все задания

--- Задача 8/Program.cs
+++ Задача 8/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Введите длину: ");
-int n = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Введите длину:");
+int n = Convert.ToInt32(Console.ReadLine());
     
 for (int i = 1; i <= n; i++)
     if (i % 2 == 0)
